70b8512c02ba379c61d57d0bebac1529ceb6db35
Rob Austein <sra@arrcus.com>
2018-11-05 07:30:31 +0000

    Address family names

--- pyroute2-test.py
+++ pyroute2-test.py
@@ -4,11 +4,14 @@
 
 import sys
 import json
+import socket
 import pyroute2
 import pyroute2.netlink.rtnl
 import tornado.ioloop
 
 from pyroute2.netlink.rtnl.ifinfmsg import IFF_UP
+
+afname = dict((getattr(socket, i), i) for i in ("AF_INET", "AF_INET6"))
 
 def handle_read(*ignored):
     msgs = ip.get()
@@ -33,22 +36,23 @@ class Interface(object):
     def show(self):
         print("Interface {0.name} (#{0.index}) link {0.macaddr}".format(self))
         for family in sorted(self.ipaddrs):
-            print("  Address family {}:".format(family))
+            print("  Address family {} (#{}):".format(afname.get(family, "???"), family))
             for ipaddr in self.ipaddrs[family]:
                 print("    {}".format(ipaddr))
 
-interfaces = {}
+ifnames = {}
+ifindex = {}
 
 with pyroute2.IPRoute() as ipr:
     for x in ipr.get_links():
         iface = Interface(x["index"], x.get_attr("IFLA_IFNAME"), x.get_attr("IFLA_ADDRESS"))
-        interfaces[iface.index] = iface
-        #interfaces[iface.name] = iface
+        ifindex[iface.index] = iface
+        ifnames[iface.name]  = iface
     for x in ipr.get_addr():
-        interfaces[x["index"]].add_ipaddr(x["family"], x.get_attr("IFA_ADDRESS"))
+        ifindex[x["index"]].add_ipaddr(x["family"], x.get_attr("IFA_ADDRESS"))
 
-for i in sorted(interfaces):
-    interfaces[i].show()
+for i in sorted(ifindex):
+    ifindex[i].show()
     print()
 
 if False:
