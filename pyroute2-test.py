8eaac97480706fbec3f5c4400d69d217bde445ee
Rob Austein <sra@arrcus.com>
2018-11-05 08:20:44 +0000

    Track interface flags

--- pyroute2-test.py
+++ pyroute2-test.py
@@ -5,27 +5,29 @@
 import sys
 import json
 import socket
+
 import pyroute2
 import pyroute2.netlink.rtnl
-import tornado.ioloop
+import pyroute2.netlink.rtnl.ifinfmsg
 
-from pyroute2.netlink.rtnl.ifinfmsg import IFF_UP
+import tornado.ioloop
 
 afname = dict((getattr(socket, i), i) for i in ("AF_INET", "AF_INET6"))
 
-def handle_read(*ignored):
-    msgs = ip.get()
-    for msg in msgs:
-        print("Interface index {} state {}".format(msg["index"],
-                                                   "up" if (msg["flags"] & IFF_UP) else "down"))
-        sys.stdout.flush()
+iface_flags = dict((name, getattr(pyroute2.netlink.rtnl.ifinfmsg, name))
+                   for name in dir(pyroute2.netlink.rtnl.ifinfmsg)
+                   if  name.startswith("IFF_")
+                   and isinstance(getattr(pyroute2.netlink.rtnl.ifinfmsg, name), int))
+
+globals().update(iface_flags)
 
 class Interface(object):
 
-    def __init__(self, index, name, macaddr):
+    def __init__(self, index, name, macaddr, flags):
         self.index   = index
         self.name    = name
         self.macaddr = macaddr
+        self.flags   = flags
         self.ipaddrs = {}
 
     def add_ipaddr(self, family, ipaddr):
@@ -33,32 +35,54 @@ class Interface(object):
             self.ipaddrs[family] = []
         self.ipaddrs[family].append(ipaddr)
 
+    @property
+    def flagtext(self):
+        return  "<{}>".format(",".join(
+            sorted(n for (n, f) in iface_flags.items() if f & self.flags != 0)))
+
+    def update_flags(self, flags):
+        old_flags = self.flagtext
+        self.flags = flags
+        print("Interface {} flags {} => {}".format(self.name, old_flags, self.flagtext))
+
     def show(self):
-        print("Interface {0.name} (#{0.index}) link {0.macaddr}".format(self))
+        print("Interface {0.name} (#{0.index}) link {0.macaddr} flags {0.flagtext}".format(self))
         for family in sorted(self.ipaddrs):
             print("  Address family {} (#{}):".format(afname.get(family, "???"), family))
             for ipaddr in self.ipaddrs[family]:
                 print("    {}".format(ipaddr))
+
+# Race condition: open event monitor socket before doing initial scans.
+# Unlikely to matter in toy demo but on busy switch it might
+
+ip = pyroute2.RawIPRoute()
+ip.bind(pyroute2.netlink.rtnl.RTNLGRP_LINK)
 
 ifnames = {}
 ifindex = {}
 
 with pyroute2.IPRoute() as ipr:
     for x in ipr.get_links():
-        iface = Interface(x["index"], x.get_attr("IFLA_IFNAME"), x.get_attr("IFLA_ADDRESS"))
+        iface = Interface(
+            index   = x["index"],
+            flags   = x["flags"],
+            name    = x.get_attr("IFLA_IFNAME"),
+            macaddr = x.get_attr("IFLA_ADDRESS"))
         ifindex[iface.index] = iface
         ifnames[iface.name]  = iface
     for x in ipr.get_addr():
-        ifindex[x["index"]].add_ipaddr(x["family"], x.get_attr("IFA_ADDRESS"))
+        ifindex[x["index"]].add_ipaddr(
+            family = x["family"],
+            ipaddr = x.get_attr("IFA_ADDRESS"))
 
 for i in sorted(ifindex):
     ifindex[i].show()
     print()
 
-if False:
-    ip = pyroute2.RawIPRoute()
-    ip.bind(pyroute2.netlink.rtnl.RTNLGRP_LINK)
+def handle_event(*ignored):
+    for msg in ip.get():
+        ifindex[msg["index"]].update_flags(msg["flags"])
 
-    ioloop = tornado.ioloop.IOLoop.current()
-    ioloop.add_handler(ip.fileno(), handle_read, tornado.ioloop.IOLoop.READ)
-    ioloop.start()
+ioloop = tornado.ioloop.IOLoop.current()
+ioloop.add_handler(ip.fileno(), handle_event, tornado.ioloop.IOLoop.READ)
+ioloop.start()
