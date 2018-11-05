423194eb0128dbcc9913ff322a79efbad7fd6e2c
Rob Austein <sra@arrcus.com>
2018-11-05 07:09:31 +0000

    Interface dump.

--- pyroute2-test.py
+++ pyroute2-test.py
@@ -1,13 +1,60 @@
 #!/usr/bin/env python3
 
-# https://docs.pyroute2.org/usage.html
+# https://docs.pyroute2.org/
 
-from pyroute2 import IPRoute
-from sys import stdout
-from json import dump
+import sys
+import json
+import pyroute2
+import pyroute2.netlink.rtnl
+import tornado.ioloop
 
-with IPRoute() as ipr:
+from pyroute2.netlink.rtnl.ifinfmsg import IFF_UP
+
+def handle_read(*ignored):
+    msgs = ip.get()
+    for msg in msgs:
+        print("Interface index {} state {}".format(msg["index"],
+                                                   "up" if (msg["flags"] & IFF_UP) else "down"))
+        sys.stdout.flush()
+
+class Interface(object):
+
+    def __init__(self, index, name, macaddr):
+        self.index   = index
+        self.name    = name
+        self.macaddr = macaddr
+        self.ipaddrs = {}
+
+    def add_ipaddr(self, family, ipaddr):
+        if family not in self.ipaddrs:
+            self.ipaddrs[family] = []
+        self.ipaddrs[family].append(ipaddr)
+
+    def show(self):
+        print("Interface {0.name} (#{0.index}) link {0.macaddr}".format(self))
+        for family in sorted(self.ipaddrs):
+            print("  Address family {}:".format(family))
+            for ipaddr in self.ipaddrs[family]:
+                print("    {}".format(ipaddr))
+
+interfaces = {}
+
+with pyroute2.IPRoute() as ipr:
     for x in ipr.get_links():
-        print(x.get_attr("IFLA_IFNAME"),
-              x.get_attr("IFLA_ADDRESS"),
-              x.get_attr("IFLA_BROADCAST"))
+        iface = Interface(x["index"], x.get_attr("IFLA_IFNAME"), x.get_attr("IFLA_ADDRESS"))
+        interfaces[iface.index] = iface
+        #interfaces[iface.name] = iface
+    for x in ipr.get_addr():
+        interfaces[x["index"]].add_ipaddr(x["family"], x.get_attr("IFA_ADDRESS"))
+
+for i in sorted(interfaces):
+    interfaces[i].show()
+    print()
+
+if False:
+    ip = pyroute2.RawIPRoute()
+    ip.bind(pyroute2.netlink.rtnl.RTNLGRP_LINK)
+
+    ioloop = tornado.ioloop.IOLoop.current()
+    ioloop.add_handler(ip.fileno(), handle_read, tornado.ioloop.IOLoop.READ)
+    ioloop.start()
