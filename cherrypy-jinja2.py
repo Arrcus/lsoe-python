15d6b72756679cbe4c5db6a90a76071684cffbce
Rob Austein <sra@hactrn.net>
2019-01-31 23:58:15 -0500

    Ye basic test

--- cherrypy-jinja2.py
+++ cherrypy-jinja2.py
@@ -8,23 +8,21 @@
 
 import cherrypy
 
-data = {}
-
 class Root(object):
 
     def __init__(self, dir = "templates"):
         from jinja2 import Environment, FileSystemLoader
         self.env = Environment(loader = FileSystemLoader(dir))
+        self.data = {}
 
     @cherrypy.expose
     def index(self):
-        return self.env.get_template("index.html").render(data)
+        return self.env.get_template("index.html").render(self.data)
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def mutate(self):
-        global data
-        data = cherrypy.request.json
+        self.data = cherrypy.request.json
 
 cherrypy.config.update({"server.socket_host" : "127.0.0.1",
                         "server.socket_port" : 8080 })
