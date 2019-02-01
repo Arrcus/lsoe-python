1ca386395c279ec6dbd30223cdd1f04347cecc3a
Rob Austein <sra@hactrn.net>
2019-01-31 23:53:33 -0500

    Hackity hack

--- cherrypy-jinja2.py
+++ cherrypy-jinja2.py
@@ -1,19 +1,32 @@
-# Minimal demo from
+#!/usr/bin/env python
+
+# Minimal demo hacked from
 # https://stackoverflow.com/questions/16844182/getting-started-with-cherrypy-and-jinja2
 
-import cherrypy
-from jinja2 import Environment, FileSystemLoader
-env = Environment(loader=FileSystemLoader('templates'))
+# In theory, this allows POSTS as JSON data to /mutate, and renders /
+# using Jinja2 templates.  Untested.
 
-class Root:
+import cherrypy
+
+data = {}
+
+class Root(object):
+
+    def __init__(self, dir = "templates"):
+        from jinja2 import Environment, FileSystemLoader
+        self.env = Environment(loader = FileSystemLoader(dir))
+
     @cherrypy.expose
     def index(self):
-        tmpl = env.get_template('index.html')
-        return tmpl.render(salutation='Hello', target='World')
+        return self.env.get_template("index.html").render(data)
 
-cherrypy.config.update({'server.socket_host': '127.0.0.1',
-                         'server.socket_port': 8080,
-                        })
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def mutate(self):
+        global data
+        data = cherrypy.request.json
+
+cherrypy.config.update({"server.socket_host" : "127.0.0.1",
+                        "server.socket_port" : 8080 })
 
 cherrypy.quickstart(Root())
-
