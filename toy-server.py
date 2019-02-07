c5b50ee5c9f94b98df0ee4960e5abc102f7c4c04
Rob Austein <sra@arrcus.com>
2019-02-07 11:31:55 -0500

    Help

--- toy-server.py
+++ toy-server.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-# Toy server using cherrypy and jinja2.
-#
-# / is the display pane, uses jinja2 to format whatever's in the toy database.
-#
-# /mutate is the upload point, parses JSON and stuffs result into the toy database.
+"""
+Toy server using cherrypy and jinja2.
+
+/ is the display pane, uses jinja2 to format whatever's in the toy database.
+
+/mutate is the upload point, parses JSON and stuffs result into the toy database.
+"""
 
 import cherrypy, argparse, jinja2
 
@@ -23,10 +25,12 @@ class Root(object):
     def mutate(self):
         self.data = cherrypy.request.json
 
-ap = argparse.ArgumentParser()
-ap.add_argument("--host", default = "127.0.0.1")
-ap.add_argument("--port", default = 8080, type = int)
-ap.add_argument("--templates", default = "templates")
+HF = type("HF", (argparse.RawDescriptionHelpFormatter,
+                 argparse.ArgumentDefaultsHelpFormatter), {})
+ap = argparse.ArgumentParser(description = __doc__, formatter_class = HF)
+ap.add_argument("--host", default = "127.0.0.1",        help = "listener address")
+ap.add_argument("--port", default = 8080, type = int,   help = "listener port")
+ap.add_argument("--templates", default = "templates",   help = "template directory")
 args = ap.parse_args()
 
 cherrypy.config.update({"server.socket_host" : args.host,
